43e19f06e250375c91b3849d99016fe569228ef8
shawal-mbalire <mbalireshawal@gmail.com>
2024-11-06 08:23:04 +0300

    nvim lazy

---
+++ .config/nvim/lua/shawal/lazy/local.lua
@@ -0,0 +1,55 @@
+
+local local_plugins = {
+    {
+        "rfceez",
+        dir = "~/personal/rfceez",
+        config = function()
+            local rfc = require("rfceez")
+            rfc.setup()
+            vim.keymap.set("n", "<leader>ra", function() rfc.add() end)
+            vim.keymap.set("n", "<leader>rd", function() rfc.rm() end)
+            vim.keymap.set("n", "<leader>rs", function() rfc.show_notes() end)
+            vim.keymap.set("n", "[r", function() rfc.nav_next() end)
+            vim.keymap.set("n", "[[r", function() rfc.show_next() end)
+        end
+    },
+    {
+        "harpoon",
+        dir = "~/personal/harpoon",
+        config = function()
+            local harpoon = require("harpoon")
+
+            harpoon:setup()
+
+            vim.keymap.set("n", "<leader>A", function() harpoon:list():prepend() end)
+            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+            vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+            vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+            vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
+            vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():replace_at(2) end)
+            vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end)
+            vim.keymap.set("n", "<leader><C-s>", function() harpoon:list():replace_at(4) end)
+        end
+    },
+    {
+        "vim-apm", dir = "~/personal/vim-apm",
+        config = function()
+            --[[
+            local apm = require("vim-apm")
+
+            apm:setup({})
+            vim.keymap.set("n", "<leader>apm", function() apm:toggle_monitor() end)
+            --]]
+        end
+    },
+    {
+        "vim-with-me", dir = "~/personal/vim-with-me",
+        config = function() end
+    },
+}
+
+return local_plugins
